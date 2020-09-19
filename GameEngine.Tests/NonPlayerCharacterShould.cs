deba8f348b530477c3407f878ef7e816ec504ed8
Krzysztof Klimczak <l2eu@wp.pl>
2020-09-19 16:20:04 +0200

    DataAtributte

--- GameEngine.Tests/NonPlayerCharacterShould.cs
+++ GameEngine.Tests/NonPlayerCharacterShould.cs
@@ -5,11 +5,12 @@ namespace GameEngine.Tests
     public class NonPlayerCharacterShould
     {                
         [Theory]
-        [MemberData(nameof(ExternalHealthDamageTestData.TestData),MemberType=typeof(ExternalHealthDamageTestData))]
+        //[MemberData(nameof(ExternalHealthDamageTestData.TestData),MemberType=typeof(ExternalHealthDamageTestData))]
         //[InlineData(0, 100)]
         //[InlineData(1, 99)]
         //[InlineData(50, 50)]
         //[InlineData(101, 1)]
+        [HealthDamageData]
         public void TakeDamage(int damage, int expectedHealth)
         {
             NonPlayerCharacter sut = new NonPlayerCharacter();
